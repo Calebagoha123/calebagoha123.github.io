058dd5c5e9ea5cdbc2f9f31f21c34b21191a337c
CalebAgoha <calebagoha@gmail.com>
2025-05-11 16:02:04 +0200

    blog added

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+    // Add date filter
+    eleventyConfig.addFilter("formatDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toFormat("dd-MM-yy");
+    });
+
+    // Configure blog post collection
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+        return collectionApi.getFilteredByGlob("blog/**/*.md");
+    });
+
+    // Pass through static assets
+    eleventyConfig.addPassthroughCopy("*.png");
+    eleventyConfig.addPassthroughCopy("*.mov");
+    eleventyConfig.addPassthroughCopy("graphs");
+
+    return {
+        dir: {
+            input: ".",
+            output: "_site",
+            includes: "_includes",
+            layouts: "_layouts"
+        }
+    };
+};
